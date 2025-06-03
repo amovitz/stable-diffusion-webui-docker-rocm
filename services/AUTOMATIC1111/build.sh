f74e4e77dc2ade775fb57e14a2f2a693584c238a
Alex Movitz <alex.movitz1@t-mobile.com>
2025-06-03 12:18:29 -0600

    Adds verbosity to builds.

--- services/AUTOMATIC1111/build.sh
+++ services/AUTOMATIC1111/build.sh
@@ -1,6 +1,17 @@
 #! /bin/bash
 
-#docker builder prune -f
+reset
+
+docker builder prune -f
 DOCKER_BUILDKIT=1 docker build \
     -t stable-diffusion-webui-rocm \
+    --progress=plain \
     --network="host" .
+
+if [ $? -ne 0 ] ; then
+    tput bel
+else
+    tput bel
+    sleep 0.5
+    tput bel
+fi
